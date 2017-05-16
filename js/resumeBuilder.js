8a99dd78713be111752d4ce321d7b0a93104129f
Suhyun Senpai <logicdevildotcom@gmail.com>
2017-05-16 07:39:18 -0400

    Added the header

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,12 @@
-$("#main").append("suhyun");
+$("#main").append(name);
+var name = "suhyun and hannah";
+var first = "hi";
+var age = 32;
+name = name.replace("and", "says");
+
+console.log(name);
+
+// what is %data%?
+// what is $("header")
+var formattedName = HTMLheaderName.replace("%data%", name);
+$("#header").append(formattedName);
